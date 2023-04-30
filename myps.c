e1e05febd436d2264dace06f0e0c4a4737bc6db3
Harshana Edirisinghe <56592096+harshanae@users.noreply.github.com>
2023-04-30 15:25:57 +0530

    next 100

---
+++ myps.c
@@ -0,0 +1,27 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define NUM_STRUCTS 10
+
+struct mystruct {
+    int value1;
+    int value2;
+};
+
+int main(void) {
+
+    struct mystruct struct_array[NUM_STRUCTS];
+    struct mystruct *p;
+
+    int return_val = mycall( NUM_STRUCTS*sizeof(struct mystruct), &struct_array);
+    p = &struct_array[0];
+
+    while(p != &struct_array[NUM_STRUCTS]){
+        printf(1,"%d  %d\n",p->value1,p->value2);
+        p++;
+    }
+
+    printf(1, "Program %d \n", return_val);
+    exit();
+}
